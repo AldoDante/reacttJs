3d5b74a65852f1e484746a69ea00a05e8bdee080
AldoDante <dante.acuario79@gmail.com>
2022-07-11 20:07:34 -0300

    Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+    <div class="nav-wrapper #61dafb blue lighten-2">
+      <a href="" class="brand-logo">COOPER´S Pizzeria</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="">Menu</a></li>
+        <li><a href="">Promos Fin de Semana</a></li>
+        <li><a href="">Contacto</a></li>
+      </ul>
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar
